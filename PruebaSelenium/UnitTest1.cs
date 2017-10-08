703527af0ad92250711f339e2ff05d2c4607e67d
Guillermo Gutierrez <guillegr123@gmail.com>
2017-10-07 22:18:05 -0600

    Cambio de sitio web utilizado para prueba.

--- PruebaSelenium/UnitTest1.cs
+++ PruebaSelenium/UnitTest1.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Threading;
 
 namespace PruebaSelenium
 {
@@ -15,13 +16,16 @@ namespace PruebaSelenium
             using (IWebDriver wdriver = new ChromeDriver())
             {
                 // Abrimos la página web de inicio se sesión
-                wdriver.Navigate().GoToUrl("https://courses.ultimateqa.com/users/sign_in");
+                wdriver.Navigate().GoToUrl("http://www.phptravels.net/admin");
 
                 // Maximizamos la ventana
                 wdriver.Manage().Window.Maximize();
 
-                // Buscamos la caja de texto del correo electrónico del usuario, por su ID
-                var cajaTextoUsuarioCorreoElectronico = wdriver.FindElement(By.Id("user_email"));
+                // Buscamos el formulario de inicio de sesión
+                var formularioInicioSesion = wdriver.FindElement(By.ClassName("form-signin"));
+
+                // Buscamos la caja de texto del correo electrónico del usuario, por nombre (name)
+                var cajaTextoUsuarioCorreoElectronico = formularioInicioSesion.FindElement(By.Name("email"));
 
                 // Hacemos clic en la caja de texto, para seleccionarla
                 cajaTextoUsuarioCorreoElectronico.Click();
@@ -30,19 +34,27 @@ namespace PruebaSelenium
                 cajaTextoUsuarioCorreoElectronico.SendKeys("usuario@prueba.com");
 
                 // Buscamos la caja de texto de la contraseña, por su ID
-                var cajaTextoContrasena = wdriver.FindElement(By.Id("user_password"));
+                var cajaTextoContrasena = formularioInicioSesion.FindElement(By.Name("password"));
 
                 // Hacemos clic en la caja de texto, para seleccionarla
                 cajaTextoContrasena.Click();
 
                 // Ingresamos la contraseña
-                cajaTextoContrasena.SendKeys("1234");
+                cajaTextoContrasena.SendKeys("contasenamala");
 
-                // Buscamos y presionamos el botón de inicio de sesión
-                wdriver.FindElement(By.Id("btn-signin")).Click();
+                // Buscamos el boton dentro del formulario
+                var botonInicioSesion = formularioInicioSesion.FindElement(By.CssSelector("button.btn[type='submit']"));
+
+                // Presionamos el botón
+                botonInicioSesion.Click();
+
+                // Debido a que el proceso de inicio se sesión es asíncrono,
+                // esperamos un tiempo prudencial a que este finalice
+                // (En este link se describe una forma apropiada de hacer esto: https://stackoverflow.com/a/7312740/806975)
+                Thread.Sleep(5000); // 5 segundos
 
                 // Verificamos que se muestra el aviso de error
-                Assert.IsTrue(ExisteElemento(wdriver, By.Id("notifications-error")));
+                Assert.IsTrue(ExisteElemento(wdriver, By.CssSelector("form.form-signin div.alert.alert-danger")));
 
                 // Cerramos el driver, y sus ventanas asociadas
                 wdriver.Quit();
